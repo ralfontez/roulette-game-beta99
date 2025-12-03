bf7ab6864c49a023551a9719bf6c05e19a9c018d
ralfontez <ralfontez@gmail.com>
2025-12-03 11:50:33 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // Obtenemos el nombre del repositorio del campo homepage
 // del package.json para usarlo como base path.
-const repoName = '/roulette-game-beta99/' // ⚠️ REEMPLAZA ESTO
+const repoName = '/roulette-game-beta50' // ⚠️ REEMPLAZA ESTO
 
 // https://vite.dev/config/
 export default defineConfig({
